a29ae0391690c1eff1f8d200db2f687d64621e2e
JJ <jj.vega86@gmail.com>
2021-02-21 13:55:01 -0500

    Adding elements to App and learning the ways HTML and JSX differ when referring to classes, JavaScript variables, and inline styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
 
 const App = () => {
-    return <div>Hi there!</div>;
-}
+    const buttonText = 'Click Me!';
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+    </div>
+  );
+};
 
 // Take the react component and show it on the screen
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
